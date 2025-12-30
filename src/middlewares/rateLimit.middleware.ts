49dd09fd2301847e63ec4d1cd5dc414c4449ee6b
MosefSanogo <mosefsanogo366@gmail.com>
2025-12-30 14:19:45 +0100

    Creation de rateLimitMiddleware pour vérifier si le nombre du texte utilisé aujourd'hui ne depasse 80 000 mots

---
+++ src/middlewares/rateLimit.middleware.ts
@@ -0,0 +1,23 @@
+import { tokenStore } from "../storage/memory.store";
+import { Request, Response, NextFunction } from "express";
+
+function rateLimitMiddleware(req: Request, res: Response, next: NextFunction) {
+  const token = (req as any).token;
+  const tokenData = tokenStore.get(token);
+
+  const today = new Date().toISOString().split('T')[0];
+    if(today !== tokenData!.lastDate) {
+      tokenData!.wordUsedToday = 0;
+      tokenData!.lastDate = today;
+    }
+    
+    const text = req.body as string;
+    const wordCount = text.trim().split(/\s+/).length;
+    if (tokenData!.wordUsedToday + wordCount > 80000) {
+        return res.status(402).send("Payment Required");
+    }
+    tokenData!.wordUsedToday += wordCount;
+    next();
+}
+
+export { rateLimitMiddleware };
